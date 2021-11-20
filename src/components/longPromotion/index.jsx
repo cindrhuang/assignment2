8abdb90e29850dfdc7c90cdc22a9d45a525263ee
cindrhuang <cindyrhuang@cmail.carleton.ca>
2021-11-19 19:05:16 -0500

    completed homepage

--- src/components/longPromotion/index.jsx
+++ src/components/longPromotion/index.jsx
@@ -1,7 +1,9 @@
 import "./styles.css";
-import ikeadining from "../../../images/IkeaDining.jpg";
+
 import { FaArrowRight } from "react-icons/fa";
 import { Button } from "../button";
+
+import ikeadining from "../../images/IkeaDining.jpg";
 
 export const LongPromo = (props) => {
     const {image, imgAlt, title, details, colour} = props;
@@ -9,7 +11,7 @@ export const LongPromo = (props) => {
     return (
         <div className="long-promo">
             <img className="long-promo-img" src={image} alt={imgAlt}/>
-            <div className="long-promo-text" style="background-color:{colour}">
+            <div className="long-promo-text" style={{backgroundColor: colour}}>
                 <h1 className="long-promo-title">{title}</h1>
                 <p className="long-promo-details">{details}</p>
                 <Button type="next" action="none" text={<FaArrowRight/>}/>
--- src/components/pages/homepage/index.jsx
+++ src/components/pages/homepage/index.jsx
@@ -1,5 +1,6 @@
 import "./styles.css";
 import { BigPromo } from "../../bigPromotion";
+import { LongPromo } from "../../longPromotion";
 
 export const Homepage = () => {
     return (
@@ -10,7 +11,18 @@ export const Homepage = () => {
                 <BigPromo className="large-promo" title={<p>Save more money this <br/>Black Friday!</p>} details="" subtitle=""/>
                 <BigPromo title="Lorem Ipsum Dolor" details="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore." subtitle={<p><b>GRÖNÖ</b> table lamp for <b>$9.99</b></p>}/>
             </div>
-            <h1>Right Now at IKEA</h1>
+
+            <div className="right-now">
+                <h1>Right Now at IKEA</h1>
+                <div className="homepage-long-promo">
+                    <LongPromo/>
+                    <LongPromo title="15% off KASER quartz kitchen countertops" details="November 11 - December 1. KASKER countertops can only be purchased in sore." colour="rgb(169, 177, 217)"/>
+                    <LongPromo title="Christmas deals are here! Get your wallets ready" details="Lorem ipsum dolor sit amet, consectetur adipiscing elit." colour="rgb(245, 191, 144)"/>
+                    <LongPromo title="Boost your bedroom with easy upgrades" details="Refresh your bedroom and bathroom with inexpensive accessories like colourful bedlinen." colour="rgb(217, 169, 200)"/>
+                    <LongPromo title="Lorem ipsum dolor sit amet, consectetur" details="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt anim id est." colour="rgb(217, 204, 106)"/>
+                    <LongPromo title="Lorem ipsum dolor sit amet, consectetur" details="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt anim id est." colour="rgb(149, 219, 205)"/>
+                </div>
+            </div>
         </div>
     )
 }
