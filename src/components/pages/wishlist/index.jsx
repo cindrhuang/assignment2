bacb8164b3a8b699ffc8ee104caa722aad5d93ee
cindrhuang <cindyrhuang@cmail.carleton.ca>
2021-11-19 20:27:51 -0500

    trying to make things look nice without breaking it

--- src/components/pages/wishlist/index.jsx
+++ src/components/pages/wishlist/index.jsx
@@ -4,23 +4,16 @@ import { WishlistItem } from "../../wishlistItem";
 export const WishlistPage = () => {
     return (
         <div className="wishlist-page">
-            <div className="wishlist-content">
-                <div className="wishlist-info">
-                    <h1>Wishlist</h1>
-                    <p>This wish list is only temporary. <u>Join or Log in</u> to make sure your wish list is still here when you come back.</p>
-                </div>
-
-                <div className="item-list">
-                    <WishlistItem key={WishlistItem.id} image={WishlistItem.image}/>
-                    <WishlistItem key={WishlistItem.id} image={WishlistItem.image}/>
-                    <WishlistItem key={WishlistItem.id} image={WishlistItem.image}/>
-                </div>
+            <div className="wishlist-info">
+                <h1>Wishlist</h1>
+                <p>This wish list is only temporary. <u>Join or Log in</u> to make sure your wish list is still here when you come back.</p>
             </div>
-            
-            {/*<div className="wishlist-summary">
-                <h2>Regular price</h2>
-                <h1>$498</h1>
-            </div>*/}
+
+            <div className="item-list">
+                <WishlistItem key={WishlistItem.id} image={WishlistItem.image}/>
+                <WishlistItem key={WishlistItem.id} image={WishlistItem.image}/>
+                <WishlistItem key={WishlistItem.id} image={WishlistItem.image}/>
+            </div>
         </div>
     )
 }
